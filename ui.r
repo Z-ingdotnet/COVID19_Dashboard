b9f8a355fa97716bdda46b49cfaa58229dd3eb4b
Z-ingdotnet <zzho044@aucklanduni.ac.nz>
2020-03-11 00:59:07 +0800

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,17 +1,3 @@
-######################################################################################
-######################################################################################
-###                                                                                ###
-###                                     TITLE:                                     ###
-###                                     R CODE                                     ###
-###                        AUTHOR: 政 ZHUZHENG(IVERSON) ZHOU                        ###
-###                                DATE: 2020-03-10                                ###
-###                                    VERSION 1                                   ###
-###  TOPIC: REAL TIME DASHBOARD FOR TRACKING THE CURRENT GLOBAL COVID-19 EPIDEMIC  ###
-###                 SOURCE: 19 COVID DATABASE FROM VARIOUS SOURCES                 ###
-###                                                                                ###
-######################################################################################
-######################################################################################
-
 library(readr)
 library(reshape2)
 library(lubridate)
@@ -26,7 +12,10 @@ library(dplyr)
 library(repmis)
 library(scatterpie)
 library(RColorBrewer)
+library(shiny.i18n)
 
+i18n <- Translator$new(translation_json_path = "./translation.json")
+i18n$set_translation_language("cn")
 
 
 test_19_covid_Confirmed<-reshape2::melt(time_series_19_covid_Confirmed,id.vars=c("Province.State","Country.Region","Lat","Long"),
@@ -60,12 +49,15 @@ all_19_covid$Headcount_active<-all_19_covid$Headcount_Confirmed-all_19_covid$Hea
 
 
 
-header <- dashboardHeader(title = "Real time dashboard for tracking the current Global Covid-19 Epidemic")  
+header <- dashboardHeader(title = i18n$t("Real time dashboard for tracking the current Global Covid-19 Epidemic"))  
+
+
+
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Contact-me", icon = icon("barcode ",lib='glyphicon'), 
+    menuItem(i18n$t("Dashboard"), tabName = "dashboard", icon = icon("dashboard")),
+    menuItem(i18n$t("Contact-me"), icon = icon("barcode ",lib='glyphicon'), 
              href = "http://z-ing.net")
   )
 )
@@ -74,38 +66,20 @@ sidebar <- dashboardSidebar(
 
 
 frow1 <- fluidRow(
-  #  column(3,
-  #         dateRangeInput("dateRange", h3("Filter Earthquake Events by date")
-  #                       , start = min(quakes_master$date), end =  max(quakes_master$date)
-  #                       , min = NULL,max = NULL
-  #                       , format = "yyyy-mm-dd",
-  #                        autoclose = TRUE
-  #          )
-  #         )
+
   
   column(3, 
-    dateInput('date',
-              label = 'Date input: yyyy-mm-dd',
+    dateInput(i18n$t('date'),
+              label = i18n$t('Date input: yyyy-mm-dd'),
               value = max(all_19_covid$date)
     )
   )
-    # column(3, 
-    #        numericInput("mag", 
-    #                     h3("Input earthquake magnitude scale"), 
-    #                     value = 5))   
-    
-  # , 
-  #  sidebarPanel(sliderInput( "magslider", h3("Select earthquake magnitude scale")
-  #                            ,min = 1,max= 10,value = c(5, 9) ))
-    
-    #valueBoxOutput("value1")
-    # ,valueBoxOutput("value2")
-    
+
   )
   
   frow11 <- fluidRow(
     box(
-      title = "Cumulative COVID-19 number",
+      title = i18n$t("Cumulative COVID-19 number"),
       color = "blue",
       ribbon = TRUE,
       collapsible = TRUE,
@@ -113,7 +87,7 @@ frow1 <- fluidRow(
       plotOutput("Cumulative_COVID_19", height = "300px")
     ),
     box(
-      title = "COVID-19 time series",
+      title = i18n$t("COVID-19 time series"),
       color = "blue",
       ribbon = TRUE,
       collapsible = TRUE,
@@ -121,7 +95,7 @@ frow1 <- fluidRow(
       plotOutput("COVID_19_timeseries", height = "300px")
     ),
     box(
-      title = "COVID-19 time series Worldwide & Mainland China",
+      title = ("COVID-19 time series Worldwide & Mainland China"),
       color = "blue",
       ribbon = TRUE,
       collapsible = TRUE,
@@ -129,7 +103,7 @@ frow1 <- fluidRow(
       plotOutput("COVID_19_timeseries_World_China", height = "300px")
     ),
     box(
-      title = "COVID-19 time series Worldwide & Mainland China(logarithmic)",
+      title = i18n$t("COVID-19 time series Worldwide & Mainland China(logarithmic)"),
       color = "blue",
       ribbon = TRUE,
       collapsible = TRUE,
@@ -159,6 +133,6 @@ frow1 <- fluidRow(
   body <- dashboardBody(frow1,frow2,frow11
   )
   
-  ui <- dashboardPage(title = 'Real time dashboard for tracking the current Global Covid-19 Epidemic', header, sidebar, body, skin='red')
+  ui <- dashboardPage(title = i18n$t('Real time dashboard for tracking the current Global Covid-19 Epidemic'), header, sidebar, body, skin='red')
   
   
