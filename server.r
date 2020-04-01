91d9a71dd80e1f58d03d01ea075b232209ebc7f6
Z-ingdotnet <zzho044@aucklanduni.ac.nz>
2020-04-01 21:40:21 +0800

    Update server.r

--- server.r
+++ server.r
@@ -13,16 +13,14 @@
 ######################################################################################
 
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
 
 
 
 shinyServer(function(input, output) {
 
+
+
+  
 library(readr)
 library(reshape2)
 library(lubridate)
@@ -34,14 +32,17 @@ library(ggplot2)
 library(ggthemes)
 library(rgeos)
 library(dplyr)
-library(repmis)
+library(repmis) 
 library(scatterpie)
 library(RColorBrewer)
 library(shiny.i18n)
+library(showtext)
+library(waiter)
+showtext_auto()
 
-  
-i18n <- Translator$new(translation_json_path = "./translation.json")
-i18n$set_translation_language("cn")
+
+
+translator <- Translator$new(translation_json_path = "./translation.json")
 
 
 test_19_covid_Confirmed<-reshape2::melt(time_series_19_covid_Confirmed,id.vars=c("Province.State","Country.Region","Lat","Long"),
@@ -78,17 +79,17 @@ all_19_covid$Headcount_active<-all_19_covid$Headcount_Confirmed-all_19_covid$Hea
 
 test_19_covid_Confirmed_bar<-reshape2::melt(time_series_19_covid_Confirmed,id.vars=c("Province.State","Country.Region","Lat","Long"),
                                         variable.name = "Calendar_Date", 
-                                        ,value.name = "Headcount"
+                                        value.name = "Headcount"
 )
 
 test_19_covid_Recovered_bar<-reshape2::melt(time_series_19_covid_Recovered,id.vars=c("Province.State","Country.Region","Lat","Long"),
                                                   variable.name = "Calendar_Date", 
-                                                  ,value.name = "Headcount"
+                                                  value.name = "Headcount"
                                                  )
 
 test_19_covid_Deaths_bar<-reshape2::melt(time_series_19_covid_Deaths,id.vars=c("Province.State","Country.Region","Lat","Long"),
                                                   variable.name = "Calendar_Date", 
-                                                  ,value.name = "Headcount"
+                                                  value.name = "Headcount"
                                                  )
 
 test_19_covid_Confirmed_bar$date<-lubridate::parse_date_time(sub("X", "", test_19_covid_Confirmed_bar$Calendar_Date), orders = "mdy")
@@ -111,15 +112,13 @@ names(test_19_covid_Active_bar)[7]<-"date"
 
 
 world <- readOGR("countries.geo.json", encoding="OGRGeoJSON", stringsAsFactors=FALSE)
-world_map <- fortify(world)
+#world_map <- fortify(world)
+world_map <- as.data.frame(merge(fortify(world), as.data.frame(world),by.x="id", by.y=0))
+#world <- gBuffer(world, byid=TRUE, width=0)
+#world <- nowrapRecenter(world)
 
-world <- gBuffer(world, byid=TRUE, width=0)
-world <- nowrapRecenter(world)
-
-test2<-as.data.frame(world_map)
-test2[which(test2$long == 33.80340), ]
-
-
+#test2<-as.data.frame(world_map)
+#test2[which(test2$long == 33.80340), ]
 
 
 
@@ -129,41 +128,68 @@ output$caption <- renderText({
 })
 
 
+output$page_content <- renderUI({
+  column(3, 
+        selectInput('selected_language',
+                    i18n()$t("Change language"),
+                    choices = translator$languages,
+                    selected = input$selected_language)
+     
+      )
+     
+})
 
+
+
+  
+#output$value1 <- renderValueBox({
+
+#all_19_covid_today<- all_19_covid %>% filter( 
+#  date.x >=input$date[1] & date.x <=input$date[1])
+
+#  valueBox(
+#    formatC(all_19_covid_today$Headcount_Confirmed, format="d", big.mark=',')
+#    ,paste('Total Confirmed')
+#    #,icon = icon("stats",lib='glyphicon')
+#    ,color = "purple")
+  
+  
+#})
 
 output$test <- renderPlot({
-  
-  
+ 
   
   all_19_covid_today<- all_19_covid %>% filter( 
     date.x >=input$date[1] & date.x <=input$date[1])
   
 
-  
   gg <- ggplot()
   gg <- gg + geom_map(data=world_map, map=world_map,
                       aes(x=long, y=lat, map_id=id),
                       color="white", size=0.15, fill="#d8d8d6")
   ##gg<- gg +
   ##  geom_point(data = test_19_covid_Confirmed, aes(x = Long, y = Lat), color = "#e60000")
-  
-  gg <- gg + geom_point(data=all_19_covid_today,
+  #gg<- gg+ geom_text(data = test_19_covid_Confirmed,aes(x =Long,  y =Lat, 
+  #                     label = Country.Region, hjust = 0, vjust = 0.5),  check_overlap = TRUE)
+  gg <- gg + geom_point(data=subset(all_19_covid_today, Headcount_Confirmed>0),
                         aes(x=Long, y=Lat, size=Headcount_Confirmed),
-                        shape=8, ##alpha=1/3
-                        , color="red" #"#d47e5d"
-                        , fill="#00000000")
-  gg <- gg + geom_point(data=subset(all_19_covid_today, Headcount_Confirmed>2000),
+                        shape=8 ##alpha=1/3
+                      , color="red" #"#d47e5d"
+                       , fill="#00000000"
+                     )
+ # gg<-gg+scale_fill_gradient2()
+  gg <- gg + geom_point(data=subset(all_19_covid_today, Headcount_Confirmed>5000),
                         aes(x=Long, y=Lat, size=Headcount_Confirmed),
                         shape=1, alpha=1, color="black", fill="#00000000")
   
   
-  gg <- gg +  geom_scatterpie(data = all_19_covid_today, 
+  gg <- gg +  geom_scatterpie(data = subset(all_19_covid_today, Headcount_Confirmed>500), 
                               aes(x=Long, y=Lat, r = sqrt(Headcount_Confirmed)/50),
                               cols = c("Headcount_active","Headcount_Recovered", "Headcount_Deaths"), 
                               alpha = 0.5) +
     scale_fill_manual(
       breaks = c("Headcount_active","Headcount_Recovered", "Headcount_Deaths"),
-      labels = c(i18n$t("Active"),i18n$t("Recovered"), i18n$t("Deaths")),
+      labels = c(i18n()$t("Active"),i18n()$t("Recovered"), i18n()$t("Deaths")),
       values = c("Headcount_Recovered" = "orange",
                  #"white" = "white",
                  "Headcount_Deaths" = "black"
@@ -172,12 +198,12 @@ output$test <- renderPlot({
     )
 
  # gg<-gg + ggtitle("Real time dashboard for tracking the current Global Covid-19 Epidemic") 
-  gg<-gg+ labs(title = i18n$t("Real time dashboard for tracking the current Global Covid-19 Epidemic"),
-               subtitle = i18n$t("19 Covid Worldwide Cases Breakdown"),
-               caption = i18n$t("Source: 19 Covid database from various sources") )
+  gg<-gg+ labs(title = i18n()$t("Real time dashboard for tracking the current Global Covid-19 Epidemic"),
+               subtitle = i18n()$t("19 Covid Worldwide Cases Breakdown"),
+               caption = i18n()$t("Source: 19 Covid database from various sources") )
   gg <- gg + scale_size(name="Confirmed Cases"#,trans='identity'
-                        ,breaks = c(10,100,500,1000,10000,20000,30000,50000,70000,80000),
-                        , labels =  c("10","100","500","1000","10000","20000","30000","50000","70000","80000")
+                        ,breaks = c(500,1000,10000,20000,30000,50000,70000,80000,100000,200000),
+                         labels =  c("500","1000","10000","20000","30000","50000","70000","80000","100000","200000")
                         ##                       ,range=c(10000, 70000)
                         ,guide = "legend")
   
@@ -198,7 +224,7 @@ output$test <- renderPlot({
   gg <- gg + theme(legend.key=element_rect(color="#00000000"))
   gg
   
-  
+
 })
 
 output$COVID_19_timeseries <- renderPlot({
@@ -208,16 +234,16 @@ output$COVID_19_timeseries <- renderPlot({
     summarise_at(vars(Headcount_Recovered,Headcount_active,Headcount_Confirmed,Headcount_Deaths),funs(sum(.,na.rm=TRUE)))
   
   gg <-  ggplot(all_19_covid_line, aes(x=as.Date(date))) 
-    gg<-gg+geom_line(aes(y=Headcount_Recovered, col=i18n$t("Recovered")), size = 2)  
-    gg<-gg+geom_line(aes(y=Headcount_active, col=i18n$t("Active")), size = 2) 
-      gg<-gg+geom_line(aes(y=Headcount_Confirmed, col=i18n$t("Confirmed")), size = 1)  
-    gg<-gg+geom_line(aes(y=Headcount_Deaths, col=i18n$t("Deaths")), size = 1) 
-    gg<-gg+scale_x_date(breaks = '1 week')  
-    gg<-gg+xlab(i18n$t("Date")) 
-    gg<-gg+ ylab(i18n$t("Number of People"))
+    gg<-gg+geom_line(aes(y=Headcount_Recovered, col=i18n()$t("Recovered")), size = 2)  
+    gg<-gg+geom_line(aes(y=Headcount_active, col=i18n()$t("Active")), size = 2) 
+      gg<-gg+geom_line(aes(y=Headcount_Confirmed, col=i18n()$t("Confirmed")), size = 1)  
+    gg<-gg+geom_line(aes(y=Headcount_Deaths, col=i18n()$t("Deaths")), size = 1) 
+    gg<-gg+scale_x_date(breaks = '1 week', date_labels = "%d-%b-%y")  
+    gg<-gg+xlab(i18n()$t("Date")) 
+    gg<-gg+ ylab(i18n()$t("Number of People"))+ scale_y_continuous(labels = scales::comma)
       gg<-gg+ scale_color_manual(name="", 
                        values = c("#e82507", "#E7B800","#0a0a0a","#22d606")) 
-  gg<-gg+theme(panel.grid.minor = element_blank()) 
+  gg<-gg+theme(panel.grid.minor = element_blank(),axis.text.x=element_text(angle=90,hjust=1)) 
   gg
   
 })
@@ -238,10 +264,10 @@ output$Cumulative_COVID_19 <- renderPlot({
   
   gg2 <-ggplot(data = all_19_covid_bar, aes(x = as.Date(date), y = Headcount,fill=Type)) 
   gg2 <-gg2+ geom_bar(stat = "identity", position = 'stack'#， position = 'dodge'
-  )+scale_fill_manual(values=qualitative)+theme_tufte(ticks = FALSE)+scale_x_date(breaks = '1 week')
+  )+scale_fill_manual(values=qualitative)+theme_tufte(ticks = FALSE)+scale_x_date(breaks = '1 week', date_labels = "%d-%b-%y")
   gg2 <-gg2#+ggtitle("Cumulative COVID-19 number")
-  gg2 <-gg2+ xlab(i18n$t("Date")) + ylab(i18n$t("Number of People"))
-  gg2<-gg2+theme(panel.grid.minor = element_blank()) 
+  gg2 <-gg2+ xlab(i18n()$t("Date")) + ylab(i18n()$t("Number of People"))+ scale_y_continuous(labels = scales::comma)
+  gg2<-gg2+theme(panel.grid.minor = element_blank(),axis.text.x=element_text(angle=90,hjust=1)) 
   gg2
   #+ scale_fill_brewer(palette = sequential)
   
@@ -259,12 +285,12 @@ output$COVID_19_timeseries_World_China <- renderPlot({
   
   all_19_covid_line_china<-all_19_covid %>%
     group_by(date) %>%
-    filter(Country.Region =="Mainland China") %>%
+    filter(Country.Region =="China") %>%
     summarise_at(vars(Headcount_Recovered,Headcount_active,Headcount_Confirmed,Headcount_Deaths),funs(sum(.,na.rm=TRUE)))
   
   all_19_covid_line_nonchina<-all_19_covid %>%
     group_by(date) %>%
-    filter(Country.Region !="Mainland China") %>%
+    filter(Country.Region !="China") %>%
     summarise_at(vars(Headcount_Recovered,Headcount_active,Headcount_Confirmed,Headcount_Deaths),funs(sum(.,na.rm=TRUE)))
   
   all_19_covid_line2<-merge(all_19_covid_line_china,all_19_covid_line_nonchina,by=c("date"))
@@ -276,16 +302,17 @@ output$COVID_19_timeseries_World_China <- renderPlot({
   
   
  gg<- ggplot(all_19_covid_line2, aes(x=as.Date(date))) 
-  gg<-gg+  geom_line(aes(y=Confirmed_china, col=i18n$t("Total Confirmed Mainland China")), size = 1.5) 
-    gg<-gg+  geom_line(aes(y=Confirmed_outsidechina, col=i18n$t("Total Confirmed Outside Mainland China")), size = 1.5) 
-    gg<-gg+  geom_line(aes(y=Headcount_active, col=i18n$t("Active Worldwide")), size = 1.5) 
-    gg<-gg+  geom_line(aes(y=Headcount_Recovered, col=i18n$t("Total Recovered Worldwide")), size = 1.5) 
+  gg<-gg+  geom_line(aes(y=Confirmed_china, col=i18n()$t("Total Confirmed Mainland China")), size = 1.5) 
+    gg<-gg+  geom_line(aes(y=Confirmed_outsidechina, col=i18n()$t("Total Confirmed Outside Mainland China")), size = 1.5) 
+    gg<-gg+  geom_line(aes(y=Headcount_active, col=i18n()$t("Active Worldwide")), size = 1.5) 
+    gg<-gg+  geom_line(aes(y=Headcount_Recovered, col=i18n()$t("Total Recovered Worldwide")), size = 1.5) 
     #ggtitle("COVID-19 timeseries Worldwide & Mainland China") 
-    gg<-gg+  xlab(i18n$t("Date")) + ylab(i18n$t("Number of People"))
-    gg<-gg+ scale_x_date(breaks = '1 week') 
+    gg<-gg+  xlab(i18n()$t("Date")) + ylab(i18n()$t("Number of People"))
+    gg<-gg+ scale_x_date( breaks = '1 week', date_labels = "%d-%b-%y") 
+    gg<-gg+ scale_y_continuous(labels = scales::comma)
     gg<-gg+ scale_color_manual(name="", 
                        values = brewer.pal(4, "PRGn")) 
-  gg<-gg+theme(panel.grid.minor = element_blank(),legend.position="bottom")
+  gg<-gg+theme(panel.grid.minor = element_blank(),legend.position="bottom",legend.text = element_text(size = 7),axis.text.x=element_text(angle=90,hjust=1))
   gg
   
 })
@@ -302,32 +329,36 @@ output$COVID_19_timeseries_World_China_log <- renderPlot({
   
   all_19_covid_line_china<-all_19_covid %>%
     group_by(date) %>%
-    filter(Country.Region =="Mainland China") %>%
+    filter(Country.Region =="China") %>%
     summarise_at(vars(Headcount_Recovered,Headcount_active,Headcount_Confirmed,Headcount_Deaths),funs(sum(.,na.rm=TRUE)))
   
   all_19_covid_line_nonchina<-all_19_covid %>%
     group_by(date) %>%
-    filter(Country.Region !="Mainland China") %>%
+    filter(Country.Region !="China") %>%
     summarise_at(vars(Headcount_Recovered,Headcount_active,Headcount_Confirmed,Headcount_Deaths),funs(sum(.,na.rm=TRUE)))
   
   all_19_covid_line2<-merge(all_19_covid_line_china,all_19_covid_line_nonchina,by=c("date"))
-  names(all_19_covid_line2)[2:3]<-c("Confirmed_china","Confirmed_outsidechina")
+  names(all_19_covid_line2)[4]<-"Confirmed_china"
+  names(all_19_covid_line2)[8] <-"Confirmed_outsidechina"
   all_19_covid_line2<-merge(all_19_covid_line[,c(1:3)],all_19_covid_line2,by=c("date"))
   
   
   
  gg<- ggplot(all_19_covid_line2, aes(x=as.Date(date))) 
-   gg<- gg+   geom_line(aes(y=log(Confirmed_china), col=i18n$t("Total Confirmed Mainland China")), size = 1.5) 
-   gg<- gg+ geom_line(aes(y=log(Confirmed_outsidechina), col=i18n$t("Total Confirmed Outside Mainland China")), size = 1.5) 
-     gg<- gg+  geom_line(aes(y=log(Headcount_active), col=i18n$t("Active Worldwide")), size = 1.5) 
-     gg<- gg+  geom_line(aes(y=log(Headcount_Recovered), col=i18n$t("Total Recovered Worldwide")), size = 1.5) 
+   gg<- gg+   geom_line(aes(y=log(Confirmed_china), col=i18n()$t("Total Confirmed Mainland China")), size = 1.5) 
+   gg<- gg+ geom_line(aes(y=log(Confirmed_outsidechina), col=i18n()$t("Total Confirmed Outside Mainland China")), size = 1.5) 
+     gg<- gg+  geom_line(aes(y=log(Headcount_active), col=i18n()$t("Active Worldwide")), size = 1.5) 
+     gg<- gg+  geom_line(aes(y=log(Headcount_Recovered), col=i18n()$t("Total Recovered Worldwide")), size = 1.5) 
     #ggtitle("COVID-19 timeseries Worldwide & Mainland China log") 
-     gg<- gg+   xlab(i18n$t("Date")) + ylab(i18n$t("Number of People"))
-     gg<- gg+  scale_x_date(breaks = '1 week') 
+     gg<- gg+   xlab(i18n()$t("Date")) + ylab(i18n()$t("Number of People"))
+     gg<- gg+  scale_x_date(breaks = '1 week', date_labels = "%d-%b-%y") 
      gg<- gg+   scale_color_manual(name="", 
                        values = brewer.pal(4, "PRGn")) 
-   gg<- gg+  theme(panel.grid.minor = element_blank(),legend.position="bottom") 
+   gg<- gg+  theme(panel.grid.minor = element_blank(),legend.position="bottom",legend.text = element_text(size = 7),axis.text.x=element_text(angle=90,hjust=1)) 
   gg
 })
 
+
+
 })
+
